b8664284b81ca3d359ea0a52edc4724261c84542
Roger Jamil Castillo Gomez <roger.castillo877@gmail.com>
2021-12-27 21:35:40 -0500

    Verify equal password

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -14,9 +14,11 @@ export class RegisterComponent {
   public registerForm = this.fb.group({
     name: ['Rocas', [Validators.required] ], 
     email: ['test@email.com', [Validators.required, Validators.email] ],
-    password: ['', [Validators.required] ],
-    password2: ['', [Validators.required] ],
-    terms: ['false', [Validators.required] ],
+    password: ['1234', [Validators.required] ],
+    password2: ['1234', [Validators.required] ],
+    terms: ['true', [Validators.required] ],
+  }, {
+    validators: this.equalPassword('password', 'password2')
   });
 
   constructor( private fb: FormBuilder) { }
@@ -26,7 +28,7 @@ export class RegisterComponent {
     console.log( this.registerForm.value );
 
     if( this.registerForm.valid ) {
-      console.log('Enciando formulario');
+      console.log('Enviando formulario');
     } else {
       console.log('Formulario inválido...');  
     }
@@ -40,9 +42,35 @@ export class RegisterComponent {
       return false;
     }
   }
+  
+  invalidpassword() {
+    const pass1 = this.registerForm.get('password')?.value;
+    const pass2 = this.registerForm.get('password2')?.value;
+
+    if( (pass1 !== pass2) && this.formSubmitted ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   termsAccepted() {
     return !this.registerForm.get('terms')?.value && this.formSubmitted;
   }
 
+  equalPassword(pass1Name: string, pass2Name: string) {
+
+    return ( formGroup: FormGroup ): any => {
+      
+      const pass1Control = formGroup.get(pass1Name);
+      const pass2Control = formGroup.get(pass2Name);
+
+      if( pass1Control!.value === pass2Control!.value ) {
+        pass2Control!.setErrors(null);
+      } else {
+        pass2Control!.setErrors({noEsIgual: true});
+      }
+
+    }
+  }
 }
